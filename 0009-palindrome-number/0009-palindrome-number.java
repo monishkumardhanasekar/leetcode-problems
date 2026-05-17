7298583d4bbe0338a7fac2ea8c88eca1a750d862
Monish Kumar Dhanasekar <145507012+monishkumardhanasekar@users.noreply.github.com>
2026-05-17 10:39:15 -0500

    Time: 5 ms (78.75%), Space: 46.1 MB (17.45%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,24 +1,45 @@
+// class Solution {
+//     public boolean isPalindrome(int x) {
+//         if (x < 0) return false;
+//         if (x == 0) return true;
+
+//         List<Integer> list = new ArrayList<>();
+
+//         while (x != 0) {
+//             list.add(x % 10);
+//             x /= 10;
+//         }
+
+//         int left = 0;
+//         int right = list.size() - 1;
+
+//         while (left < right) {
+//             if (!list.get(left).equals(list.get(right))) return false;
+//             left++;
+//             right--;
+//         }
+
+//         return true;
+//     }
+    
+// }
+
 class Solution {
-    public boolean isPalindrome(int x) {
-        if (x < 0) return false;
-        if (x == 0) return true;
-
-        List<Integer> list = new ArrayList<>();
-
-        while (x != 0) {
-            list.add(x % 10);
-            x /= 10;
+    public boolean isPalindrome(int x) 
+    {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
         }
 
-        int left = 0;
-        int right = list.size() - 1;
+        int revertedNumber = 0;
+        int originalNumber = x;
 
-        while (left < right) {
-            if (!list.get(left).equals(list.get(right))) return false;
-            left++;
-            right--;
+        while (x > 0) {
+            int lastDigit = x % 10;
+            revertedNumber = revertedNumber * 10 + lastDigit;
+            x = x/10;
         }
 
-        return true;
+        return originalNumber == revertedNumber;
     }
 }
