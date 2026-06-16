ba6ac4e2e5f6adab22a5b8036e706331be2acb96
Monish Kumar Dhanasekar <145507012+monishkumardhanasekar@users.noreply.github.com>
2026-06-16 09:41:43 -0500

    Time: 4 ms (76.47%), Space: 55 MB (75%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder res = new StringBuilder(); 
+
+        for (char ch: s.toCharArray()){
+            if(ch == '#'){
+                res.append(res);
+            }
+            else if(ch == '*'){
+                if (res.length() > 0) {
+                    res.deleteCharAt(res.length() - 1); 
+                }
+            }
+            else if(ch == '%'){
+                res.reverse(); 
+            } else{
+                res.append(ch);
+            }
+
+        }
+
+        return res.toString();
+
+        
+    }
+}
