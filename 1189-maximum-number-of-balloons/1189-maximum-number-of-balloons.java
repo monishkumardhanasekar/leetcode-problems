c27eaaadd76997cfbb43e3585a1cf5690e337e60
Monish Kumar Dhanasekar <145507012+monishkumardhanasekar@users.noreply.github.com>
2026-06-22 10:06:38 -0500

    Time: 10 ms (43.98%), Space: 44.2 MB (14.36%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -5,7 +5,6 @@ class Solution {
         for(char t: text.toCharArray()){
             freq.put(t, freq.getOrDefault(t,0) + 1);
         }
-        System.out.println(freq);
         
         int min = Math.min( freq.getOrDefault('b', 0),Math.min(freq.getOrDefault('a', 0),
         Math.min(freq.getOrDefault('n', 0),Math.min( freq.getOrDefault('l', 0) / 2, freq.getOrDefault('o', 0) / 2))));
